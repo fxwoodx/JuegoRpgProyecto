385ee998192c4a1c81a138c72bb6a0e0e44550da
RobertXLarge <73672660+RobertXLarge@users.noreply.github.com>
2021-07-03 05:39:39 -0700

    Add files via upload

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void BotonStart()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    public void BotonSalir()
+    {
+        Debug.Log("Aplicacion cerrada");
+        Application.Quit();
+    }
+
+}
